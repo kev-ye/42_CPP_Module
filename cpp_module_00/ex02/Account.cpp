c22b3759663281422d950a67865c59edcd65f889
Kangkai YE <kaye@e2r8p5.42.fr>
2021-07-25 16:06:11 +0200

    ex02 done

--- cpp_module_00/ex02/Account.cpp
+++ cpp_module_00/ex02/Account.cpp
@@ -6,20 +6,41 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 15:43:00 by kaye              #+#    #+#             */
-/*   Updated: 2021/07/23 16:27:16 by kaye             ###   ########.fr       */
+/*   Updated: 2021/07/25 15:55:37 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
 
-Account::Account(int initial_deposit) {
-	
-	initial_deposit = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) :
+	_accountIndex(Account::_nbAccounts),
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0) {
+
+	Account::_nbAccounts++;
+	Account::_totalAmount += this->_amount;
+
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ';' \
+		<< "amount:" << this->_amount << ';' \
+		<< "created" << std::endl; 
 	return ;
 }
 
 Account::~Account(void) {
-		
+	
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ';' \
+		<< "amount:" << this->_amount << ';' \
+		<< "closed" << std::endl;
 	return ;
 }
 
@@ -45,27 +66,94 @@ int		Account::getNbWithdrawals(void) {
 
 void	Account::displayAccountsInfos(void) {
 	
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::getNbAccounts() << ';' \
+		<< "total:" << Account::getTotalAmount() << ';' \
+		<< "deposits:" << Account::getNbDeposits() << ';' \
+		<< "withdrawals:" << Account::getNbWithdrawals() << std::endl;
 }
 
 void	Account::makeDeposit(int deposit) {
 	
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ';' \
+		<< "p_amout:" << this->_amount << ';' \
+		<< "deposits:" << deposit << ';';
+	
+	this->_nbDeposits = 1;
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits += this->_nbDeposits;
+	std::cout << "amout:" << this->_amount << ';' \
+		<< "nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal(int withdrawal) {
 	
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ';' \
+		<< "p_amount:" << this->_amount << ';';
+
+	if (withdrawal > this->checkAmount()) {
+		
+		this->_nbWithdrawals = 0;
+		std::cout << "withdrawal:refused" << std::endl;
+		return (false);
+	}
+	
+	std::cout << "withdrawl:" << withdrawal << ';';
+
+	this->_nbWithdrawals = 1;
+	this->_amount -= withdrawal;
+	Account::_totalAmount -= withdrawal;
+	Account::_totalNbWithdrawals += this->_nbWithdrawals;
+	std::cout << "amount:" << this->_amount << ';' \
+		<< "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
 	return (true);
 }
 
 int		Account::checkAmount(void) const {
 	
-	return (0);
+	return (this->_amount);
 }
 
 void	Account::displayStatus(void) const {
 	
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ';' \
+		<< "amount:" << this->_amount << ';' \
+		<< "deposits:" << this->_nbDeposits << ';' \
+		<< "withdrawals:" << this->_nbWithdrawals<< std::endl; 
 }
 
-void		_displayTimetamp(void) {
+void	Account::_displayTimestamp(void) {
 	
+	const time_t	now = time(0);
+	const tm		*ltm = localtime(&now);
 	
+	std::cout << '[';
+
+	// date
+	std::cout << 1900 + ltm->tm_year;
+	if ((ltm->tm_mon + 1) < 10)
+		std::cout << '0';
+	std::cout << ltm->tm_mon + 1;
+	if (ltm->tm_mday < 10)
+		std::cout << '0';
+	std::cout << ltm->tm_mday;
+
+	std::cout << '_';
+
+	// time
+	if (ltm->tm_hour < 10)
+		std::cout << '0';
+	std::cout << ltm->tm_hour;
+	if (ltm->tm_min < 10)
+		std::cout << '0';
+	std::cout << ltm->tm_min;
+	if (ltm->tm_sec < 10)
+		std::cout << '0';
+	std::cout << ltm->tm_sec;
+
+	std::cout << "] ";
 }

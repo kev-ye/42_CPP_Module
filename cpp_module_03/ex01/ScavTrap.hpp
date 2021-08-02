7461f07d29a882c779bf9898211bfbba3b981bfe
Kangkai YE <kaye@e1r9p20.42.fr>
2021-08-02 14:42:40 +0200

    module 03 ex00 progressing

---
+++ cpp_module_03/ex01/ScavTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 13:02:55 by kaye              #+#    #+#             */
+/*   Updated: 2021/08/02 14:33:21 by kaye             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap(void);
+		~ScavTrap(void);
+
+	private:
+		/* data */
+};
+
+#endif

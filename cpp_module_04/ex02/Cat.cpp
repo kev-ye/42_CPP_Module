07d1907c9389d616a7b4a367de3bbff3a2204749
Kangkai YE <kaye@e1r8p4.42.fr>
2021-08-07 18:46:09 +0200

    04 ex02 + ex03 probably done + ex04 progressing

---
+++ cpp_module_04/ex02/Cat.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/05 12:47:23 by kaye              #+#    #+#             */
+/*   Updated: 2021/08/07 18:41:54 by kaye             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat(void) :
+	Animal("Cat"),
+	_brain(new Brain()) {
+	std::cout << "\e[1;34mCat\e[0m default constructor" << std::endl;
+	return ;
+}
+
+Cat::Cat(Cat const & src) :
+	Animal(src),
+	_brain(new Brain(*(src._brain))) {
+	std::cout << "\e[1;34mCat\e[0m assignment constructor" << std::endl;
+	return ;
+}
+
+Cat::~Cat(void) {
+	std::cout << "\e[1;34mCat\e[0m destructor" << std::endl;
+	delete this->_brain;
+	return ;
+}
+
+void	Cat::makeSound(void) const {
+	std::cout << "Miaouh miaouh ..." << std::endl;
+}
+
+Brain	*Cat::getBrain(void) const {
+	return this->_brain;
+}
+
+Cat & Cat::operator=(Cat const & rhs) {
+	if (this != &rhs) {
+		this->_type = rhs._type;
+		this->_brain = new Brain(*(rhs._brain));
+	}
+	return *this;
+}
---
+++ cpp_module_04/ex02/Dog.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/05 12:47:23 by kaye              #+#    #+#             */
+/*   Updated: 2021/08/07 14:41:37 by kaye             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog(void) :
+	Animal("Oog"),
+	_brain(new Brain()) {
+	std::cout << "\e[1;36mDog\e[0m default constructor" << std::endl;
+	return ;
+}
+
+Dog::Dog(Dog const & src) :
+	Animal(src),
+	_brain(new Brain(*(src._brain))) {
+	std::cout << "\e[1;36mDog\e[0m assignment constructor" << std::endl;
+	return ;
+}
+
+Dog::~Dog(void) {
+	std::cout << "\e[1;36mDog\e[0m destructor" << std::endl;
+	delete this->_brain;
+	return ;
+}
+
+void	Dog::makeSound(void) const {
+	std::cout << "Wof wof ..." << std::endl;
+}
+
+Brain	*Dog::getBrain(void) const {
+	return this->_brain;
+}
+
+Dog & Dog::operator=(Dog const & rhs) {
+	if (this != &rhs) {
+		this->_type = rhs._type;
+		this->_brain = new Brain(*(rhs._brain));
+	}
+	return *this;
+}

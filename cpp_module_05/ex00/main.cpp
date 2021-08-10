28c700b7ea7f4ba02391df6f64b28ca4303074da
Kangkai YE <kaye@e1r9p3.42.fr>
2021-08-10 18:17:19 +0200

    03: add linux mode + 04: ex00 progressing

--- cpp_module_05/ex00/main.cpp
+++ cpp_module_05/ex00/main.cpp
@@ -6,16 +6,23 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 17:53:18 by kaye              #+#    #+#             */
-/*   Updated: 2021/08/10 15:36:54 by kaye             ###   ########.fr       */
+/*   Updated: 2021/08/10 18:14:51 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
-	Bureaucrat bureaucrat("bureaucrat", 1);
-	bureaucrat.incGrade();
+	// Bureaucrat bureaucrat("bureaucrat", 1);
+	// bureaucrat.incGrade();
+	try {
+		Bureaucrat bureaucrat("bureaucrat", 0);
+		std::cout << bureaucrat << std::endl;
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+		return 0;
+	}
 
-	std::cout << bureaucrat << std::endl;
 	return (0);
 }
